be6d281067015207221009a890a9b6023b574d06
NimaBavar <khodenimacollab@gmail.com>
2025-03-09 18:21:29 +0330

    feat(window): Set up main game window Main.Project: 3

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Main AlienInvasion game module
+By: NimaDaniels
+2025/3/9
+khodenimacollab@gmail.com
+"""
+
+
+import sys
+import pygame
+
+
+class AlienInvasion:
+    def __init__(self) -> None:
+        """Main class to manage game assets and behaviors."""
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien: Invasion ( By NimaDaniels )")
+
+    def run_game(self) -> None:
+        """Start main game loop"""
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    alien_game = AlienInvasion()
+    alien_game.run_game()
