0c3f114494d02e9bb7ed14182f61f70d9bb85437
NimaBavar <khodenimacollab@gmail.com>
2025-03-10 18:12:10 +0330

    feat(main): make base settings settings: 2

---
+++ settings.py
@@ -0,0 +1,12 @@
+"""
+Module for the game's Settings class.
+"""
+
+
+class Settings:
+    """Class to store all settings for Alien Invasion"""
+
+    def __init__(self) -> None:
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.background_color = (230, 230, 230)
